4aa561ca370a2e3c9ac919e8c6e40cadc1d32486
Stéphane Klein <contact@stephane-klein.info>
2024-01-25 09:51:07 +0100

    Add comment

--- test1.sh
+++ test1.sh
@@ -1,2 +1,5 @@
 docker compose run -T --rm test1 ls
+
+# Pourquoi cette ligne n'est jamais lancée ? Si je remplace docker compose run par toute autre commande, tout se passe bien. docker compose run semble effectuer quelque chose de bizarre
+# J'ai bien vérifié que docker compose run retourne bien un exit 0
 echo "hello"
