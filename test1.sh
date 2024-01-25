6d3b3fd0b8f7be5f9405d5b3f0101e8e833256d4
Stéphane Klein <contact@stephane-klein.info>
2024-01-25 10:37:50 +0100

    Fix with --interactive=false workaround

--- test1.sh
+++ test1.sh
@@ -1,4 +1,5 @@
-docker compose run -T --rm test1 ls
+# Fixed with "--interactive=false", found in https://github.com/docker/compose/issues/10418#issuecomment-1491426721
+docker compose run -T --interactive=false --rm test1 ls
 
 # Pourquoi cette ligne n'est jamais lancée ? Si je remplace docker compose run par toute autre commande, tout se passe bien. docker compose run semble effectuer quelque chose de bizarre
 # J'ai bien vérifié que docker compose run retourne bien un exit 0
